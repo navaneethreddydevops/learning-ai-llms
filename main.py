f68bd6ae14fd069d2d9a5068267e66d6d5da7d4f
Navaneeth Reddy Alavala <navaneethreddydevops@gmail.com>
2023-10-17 01:44:26 -0500

    Adding the local chat language model

--- main.py
+++ main.py
@@ -1,2 +1,34 @@
-if __name__ == '__main__':
-    print("Hello Langchain")
+import os
+from dotenv import load_dotenv
+from huggingface_hub import hf_hub_download
+from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline, AutoModelForSeq2SeqLM
+
+# Loading the .env environment variables
+load_dotenv()
+HUGGING_FACE_API_KEY = os.environ.get("HUGGING_FACE_API_KEY")
+model_id = "lmsys/fastchat-t5-3b-v1.0"
+file_names = [
+    "pytorch_model.bin",
+    "added_tokens.json",
+    "config.json",
+    "generation_config.json",
+    "special_tokens_map.json",
+    "tokenizer_config.json",
+    "spiece.model"
+]
+
+for filename in file_names:
+    downloaded_model_path = hf_hub_download(
+        repo_id=model_id,
+        filename=filename,
+        token=HUGGING_FACE_API_KEY
+    )
+    print(downloaded_model_path)
+
+tokenizer = AutoTokenizer.from_pretrained(model_id, legacy=False)
+model = AutoModelForSeq2SeqLM.from_pretrained(model_id)
+
+pipeline = pipeline("text2text-generation", model=model, device=-1, tokenizer=tokenizer, max_length=1000)
+
+result = pipeline("Who is narendra modi")
+print(result)
